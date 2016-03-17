c0c706457f509043ae976bdc673036f0e6a99bab
NazarOps <lilnz4@hotmail.com>
2016-03-17 13:38:24 +0100

    Added files via upload

---
+++ navigation.php
@@ -0,0 +1,5 @@
+	<a href="test.html"><div class="button"> <p> Hem </p> </div></a>
+	<a href="flik2.html"><div class="button"> <p> <font color=7D7D7D>Flik 2</font> </p> </div></a>
+	<a href="flik3.html"><div class="button"><p> Flik 3</p></div></a>
+	<a href="flik4.html"><div class="button"><p> Flik 4</p></div></a>
+	
---
+++ test.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<title>Gameshop</title>
+<head>
+<link rel="Stylesheet" href="css.css">
+
+</head>
+
+
+<body>
+
+<div class="navigation">
+	<?php
+	include("navigation.php");
+	?>
+</div> 
+<!-- slut på navigation -->
+
+
+<div class="main">
+
+
+<center><h1>Rea</h1></center>
+
+</div>
+
+
+<div class="footer">
+
+
+
+
+</div>
+
+</div>
+</body>
+
+</html>
